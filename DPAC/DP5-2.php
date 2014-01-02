c06b45180ce6900e2baa9f8e0478558e01c75e0e
tonycoll91 <tonycoll91@yahoo.com>
2014-01-02 13:11:59 -0500

    Discovery Projects
    project 5-2 added images

---
+++ DPAC/DP5-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE hmtl>
+<html>
+<head>
+	<title>Chinese Zodiac Using IF ELSE</title>
+	<meta charset="utf-8" />
+<body>
+	<h1>THE CHINESE ZODIAC</h1>
+	<h3>Using IF ELSE</h3>
+	<div>Birth Information
+		<p>Year of Birth:</p>
+	</div>
+	<div>Reset and Submit Information
+	</div>
+</body>
+</html>
