f4c2ea9af5a2bef14b1c362ea97448dbaae15ba8
tonycoll91 <tonycoll91@yahoo.com>
2014-01-08 00:44:28 -0500

    got the random proverb to work

--- DPAC/5-5/UploadProverb.php
+++ DPAC/5-5/UploadProverb.php
@@ -1,35 +1,34 @@
 <!DOCTYPE HTML5>
 <html>
 <head>
-<title></title>
-<meta charset="utf-8" />
-<style>
-	body{
-		background: grey;
-	}
-	textarea{
-		resize: none;
-		height: 300px;
-		width: 600px;
-		font-size: 13pt;
-	}
-</style>
-<script src="jquery-1.6.1.min.js"></script>
-<script>
-$(document).ready(function(){
-	$(input).onclick(function(){
-		$(#text).val('');
-	});
-});
-</script>
-
+	<title>Ancient Chinese Proverb</title>
+	<meta charset="utf-8" />
+	<style>
+		body{
+			background: grey;
+		}
+		textarea{
+			resize: none;
+			height: 300px;
+			width: 600px;
+			font-size: 13pt;
+		}
+		h1,h2{
+			text-align: center;
+		}
+	</style>
 </head>
+
 <body>
+	<h1>Type Your Proverb</h1>
+	
 	<form align="center" action="<?php echo $_SERVER['SCRIPT_NAME'];?>" method="post">
 		<textarea id="text" type="commentbox" name="userproverb" ></textarea></br>
 		<input type="submit" name="submit"  value="Add Chinese Proverb" />
 	</form>
+	
 <?php
+
 	if (isset($_POST['submit']))
 	{
 		$file = fopen("proverbs.txt","a+");
@@ -37,10 +36,17 @@ $(document).ready(function(){
 		fwrite($file, $new_content. "%"."\n");
 		fclose($file);
 	}
+	
 ?>
 
+	<h2>Random Proverb Will Appear on Page Reload</h2>
+	
 <?php
-	explode("%", "proverb.txt");
+	$proverbs = "proverbs.txt";
+	$proverb = file_get_contents($proverbs);
+	$proverb = explode("%", $proverb);
+	$ranNum = rand(0, (count($proverb)-2));	
+	echo "<p style='text-align: center;'>$proverb[$ranNum]</p>";
 ?>
 </body>
 </html>
